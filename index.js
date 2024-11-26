a077c47a562c38f36214cb25b8ffcf49f9797348
mdmonauwarulislam <mdmonauwarulislam@gmail.com>
2024-11-26 19:47:59 +0530

    fixing for deployement

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-const app = require('./src/app');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const urlRoutes = require('./src/routes/urlRoutes');
+const rateLimiter = require('./src/middleware/rateLimited');
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(rateLimiter);
+
+mongoose.connect(process.env.MONGO_URI, {
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+app.use('/', urlRoutes);
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
